20e38cf7c346b261c001ac43b6f7a14bc2ce3d5a
mackliu <mackliu@wda.gov.tw>
2022-11-14 14:45:38 +0800

    讓科系選單從資料庫撈取資料來產生,而不是頁面寫死

--- add.php
+++ add.php
@@ -1,3 +1,8 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,'root','');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +13,69 @@
 </head>
 <body>
 <h1>新增學生</h1>
+
+<form action="api/add_student.php" method="post">
+    <table>
+        <tr>
+            <td>school_num</td>
+            <td><input type="text" name="school_num"></td>
+        </tr>
+        <tr>
+            <td>name</td>
+            <td><input type="text" name="name"></td>
+        </tr>
+        <tr>
+            <td>birthday</td>
+            <td><input type="date" name="birthday"></td>
+        </tr>
+        <tr>
+            <td>uni_id</td>
+            <td><input type="text" name="uni_id"></td>
+        </tr>
+        <tr>
+            <td>addr</td>
+            <td><input type="text" name="addr"></td>
+        </tr>
+        <tr>
+            <td>parents</td>
+            <td><input type="text" name="parents"></td>
+        </tr>
+        <tr>
+            <td>tel</td>
+            <td><input type="text" name="tel"></td>
+        </tr>
+        <tr>
+            <td>dept</td>
+            <td>
+                <select name="dept">
+                    <?php
+                        $sql="SELECT * FROM `dept`";
+                        $depts=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                        foreach($depts as $dept){
+                            echo "<option value='{$dept['id']}'>{$dept['name']}</option>";
+                        }
+                    ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>graduate_at</td>
+            <td><input type="number" name="graduate_at"></td>
+        </tr>
+        <tr>
+            <td>status_code</td>
+            <td><input type="text" name="status_code"></td>
+        </tr>
+        <tr>
+            <td>班級</td>
+            <td><input type="text" name="classes"></td>
+        </tr>
+        <tr>
+            <td>座號</td>
+            <td><input type="number" name="seat_num"></td>
+        </tr>
+    </table>
+    <input type="submit" value="確認新增">
+</form>
 </body>
 </html>
