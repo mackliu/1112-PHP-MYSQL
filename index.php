52ab4849d43528b70f327a29db28c857e5353b26
mackliu <mackliu@wda.gov.tw>
2022-11-14 11:16:19 +0800

    增加年齡計算及美化列表

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,'root','');
 
-$sql="SELECT * FROM `students` LIMIT 5";
+$sql="SELECT * FROM `students` LIMIT 20";
 
 $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -20,11 +20,27 @@ $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 <body>
 <h1>學生管理系統</h1>
 
+<table class='list-students'>
+<tr>
+    <td>學號</td>
+    <td>姓名</td>
+    <td>生日</td>
+    <td>畢業國中</td>
+    <td>年齡</td>
+</tr>    
 <?php
-echo "<pre>";
-print_r($rows);
-echo "</pre>";
-
+foreach($rows as $row){ 
+    $age=round((strtotime('now')-strtotime($row['birthday']))/(60*60*24*365),1);
+    
+ echo "<tr>";
+ echo "<td>{$row['school_num']}</td>";
+ echo "<td>{$row['name']}</td>";
+ echo "<td>{$row['birthday']}</td>";
+ echo "<td>{$row['graduate_at']}</td>";
+ echo "<td>{$age}</td>";
+ echo "</tr>";
+}
 ?>
+</table>
 </body>
 </html>
