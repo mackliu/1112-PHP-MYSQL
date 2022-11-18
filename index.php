45a7b19750f6fcae6b9828ba9c8af5a8549820c2
mackliu <mackliu@wda.gov.tw>
2022-11-18 15:51:27 +0800

    首頁增加分班級顯示功能

--- index.php
+++ index.php
@@ -12,20 +12,52 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,'root','');
 
-//建立撈取學生資料的語法，限制只撈取前20筆
-$sql="SELECT * FROM `students` LIMIT 20";
+if(isset($_GET['code'])){
+    $sql="SELECT `students`.`id` as 'id' ,
+                `students`.`school_num` as '學號',
+                 `students`.`name` as '姓名',
+                 `students`.`birthday` as '生日',
+                 `students`.`graduate_at` as '畢業國中'
+          FROM `class_student`,`students` 
+          WHERE `class_student`.`school_num`=`students`.`school_num` && 
+                `class_student`.`class_code`='{$_GET['code']}'";
+}else{
+    //建立撈取學生資料的語法，限制只撈取前20筆
+    $sql="SELECT `students`.`id` as 'id' ,
+                 `students`.`school_num` as '學號',
+                 `students`.`name` as '姓名',
+                 `students`.`birthday` as '生日',
+                 `students`.`graduate_at` as '畢業國中'
+          FROM `students` LIMIT 20";
+}
 
 //執行SQL語法，並從資料庫取回全部符合的資料，加上PDO::FETCH_ASSOC表示只需回傳帶有欄位名的資料
 $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 </head>
 <body>
+<!-- <pre>
+   <?php //print_r($rows);?> ;
+</pre> -->
 <h1>學生管理系統</h1>
 <nav>
     <a href="add.php">新增學生</a>
     <a href="reg.php">教師註冊</a>
     <a href="login.php">教師登入</a>
 
+</nav>
+<nav>
+<ul class="class-list">
+<?php
+    //從`classes`資料表中撈出所有的班級資料並在網頁上製作成下拉選單的項目
+    $sql="SELECT `id`,`code`,`name` FROM `classes`";
+    $classes=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    foreach($classes as $class){
+        echo "<li><a href='?code={$class['code']}'>{$class['name']}</a></li>";
+    }
+?>  
+</ul>
 </nav>
 <!--根據status來顯示回應-->
 <?php
@@ -57,13 +89,13 @@ if(isset($_GET['status'])){
 <?php
 //使用迴圈來顯示每一位學生的資料
 foreach($rows as $row){ 
-    $age=round((strtotime('now')-strtotime($row['birthday']))/(60*60*24*365),1);
+    $age=round((strtotime('now')-strtotime($row['生日']))/(60*60*24*365),1);
     
  echo "<tr>";
- echo "<td>{$row['school_num']}</td>";
- echo "<td>{$row['name']}</td>";
- echo "<td>{$row['birthday']}</td>";
- echo "<td>{$row['graduate_at']}</td>";
+ echo "<td>{$row['學號']}</td>";
+ echo "<td>{$row['姓名']}</td>";
+ echo "<td>{$row['生日']}</td>";
+ echo "<td>{$row['畢業國中']}</td>";
  echo "<td>{$age}</td>";
  echo "<td>";
  //加上連結將頁面導向edit.php，同時以GET方式將學生資料的id傳遞到edit.php
