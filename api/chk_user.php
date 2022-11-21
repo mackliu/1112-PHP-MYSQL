583c37472d887caf2bb8ad2004669b1ea0b4f197
mackliu <mackliu@wda.gov.tw>
2022-11-21 16:14:10 +0800

    增加會員登入檢查及SESSION使用

---
+++ api/chk_user.php
@@ -0,0 +1,26 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,'root','');
+
+session_start();
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$sql="select count(`id`) from `users` where `acc`='$acc' && `pw`='$pw' ";
+$chk=$pdo->query($sql)->fetchColumn();
+
+if($chk==1){
+    $sql="select `id`,`acc`,`name`,`last_login` from `users` where `acc`='$acc' && `pw`='$pw' ";
+    $user=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+
+    $_SESSION['login']=$user;
+}else{
+    if(isset($_SESSION['login_try'])){
+        $_SESSION['login_try']++;
+    }else{
+        $_SESSION['login_try']=1;
+    }
+    header("location:../login.php?error=login");
+}
+?>
--- login.php
+++ login.php
@@ -8,5 +8,22 @@
 </head>
 <body>
 <h1>教師登入</h1>
+<div>
+<?php
+session_start();
+
+if(isset($_GET['error'])){
+    echo "帳號或密碼錯誤，";
+    echo "登入嘗試".$_SESSION['login_try']."次";
+}
+
+?>
+</div>
+
+<form action="./api/chk_user.php" method="post">
+    <div>帳號:<input type="text" name="acc" id=""></div>
+    <div>密碼:<input type="password" name="pw" id=""></div>
+    <div><input type="submit" value="登入" id=""></div>
+</form>
 </body>
 </html>
