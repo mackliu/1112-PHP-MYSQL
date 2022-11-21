52c73fb3373ad78add6584363ea870e1ea142c52
mackliu <mackliu@wda.gov.tw>
2022-11-21 15:22:48 +0800

    完成新增資料到使用者資料表

--- api/reg_user.php
+++ api/reg_user.php
@@ -6,7 +6,9 @@ $acc=trim(strip_tags($_POST['acc']));
 $pw=trim($_POST['pw']);
 $name=trim($_POST['name']);
 $email=trim($_POST['email']);
+$last_login=null;
 
+$sql="insert into `users` (`acc`,`pw`,`name`,`email`,`last_login`) values('$acc','$pw','$name','$email','$last_login')";
 echo "acc=>".$acc;
 echo "<br>";
 echo "pw=>".$pw;
@@ -15,4 +17,6 @@ echo "name=>".$name;
 echo "<br>";
 echo "email=>".$email;
 echo "<br>";
+$pdo->exec($sql);
+header("location:../login.php");
 
