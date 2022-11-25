1143b7ce7753f605381bafd9e4717c6781a31090
mackliu <mackliu@wda.gov.tw>
2022-11-25 11:27:43 +0800

    拆出管理後台的主要內容區

--- index.php
+++ index.php
@@ -9,17 +9,12 @@ include "./db/base.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>學生管理系統</title>
     <link rel="stylesheet" href="style.css">
-
-
 </head>
 <body>
 <?php
     include "./layouts/header.php";
 ?>
 
-<!-- <pre>
-   <?php //print_r($rows);?> ;
-</pre> -->
 <h1 style='text-align:center'>學生管理系統</h1>
 <nav>
     <a href="reg.php">教師註冊</a>
@@ -31,23 +26,6 @@ include "./db/base.php";
 ?>  
 <?php
     include "./front/main.php";
-?>
-<!--根據status來顯示回應-->
-<?php
-/* if(isset($_GET['status'])){
-    switch($_GET['status']){
-        case 'add_success':
-            echo "<span style='color:green'>新增學生成功</span>";
-        break;
-        case 'add_fail';
-            echo "<span style='color:red'>新增學生有誤</span>";
-        break;
-        case 'edit_error':
-            echo "<span style='color:red'>無法編輯，請洽管理員，或正確操作</span>";
-        break;
-    }
-}
- */
 ?>
 
 </body>
