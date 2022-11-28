2eb42f1edf704a47dea0e36ac958b779ad92128b
mackliu <mackliu@wda.gov.tw>
2022-11-28 15:20:41 +0800

    調整新聞列表的顯示內容,前後台不同

--- back/news.php
+++ back/news.php
@@ -2,17 +2,33 @@
 <a class="btn btn-primary" href="admin_center.php?do=add_news">新增消息</a>
 <hr>
 <ul class="list-group">
-
+    <li class='list-group-item list-group-item-action d-flex text-center bg-info text-white'>
+        <div class='col-md-6'>標題</div>
+        <div class='col-md-1'>置頂</div>
+        <div class='col-md-1'>點閱數</div>
+        <div class='col-md-2'>發佈時間</div>
+        <div class='col-md-2'>操作</div>
+    </li>
 <?php
 $all_news="SELECT * FROM `news` ";
 $rows=$pdo->query($all_news)->fetchAll();
 
 foreach($rows as $row){
     echo "<li class='list-group-item list-group-item-action d-flex'>";
-    echo "<div class='col-md-8'>";
+    echo "<div class='col-md-6'>";
     echo $row['subject'];
     echo "</div>";
-    echo "<div class='col-md-4 text-center'>";
+    echo "<div class='col-md-1'>";
+    echo ($row['top']==1)?"TOP":"";
+    echo "</div>";
+    echo "<div class='col-md-1'>";
+    echo $row['readed'];
+    echo "</div>";
+    echo "<div class='col-md-2'>";
+    echo $row['created_at'];
+    echo "</div>";
+
+    echo "<div class='col-md-2 text-center'>";
     echo "<a class='btn btn-info mx-2' href='admin_center.php?do=news_edit&id={$row['id']}'>編輯</a>";
     echo "<a class='btn btn-danger mx-2' href='admin_center.php?do=news_del&id={$row['id']}'>刪除</a>";
     echo "</div>";
--- front/main.php
+++ front/main.php
@@ -1,14 +1,23 @@
 <h1 class="text-center">最新消息</h1>
 <ul class="list-group">
+<li class='list-group-item list-group-item-action d-flex text-center bg-info text-white'>
+    <div class='col-md-10'>標題</div>
+    <div class='col-md-2'>人氣</div>
+</li>    
 <?php
 $all_news="SELECT * FROM `news` ";
 $rows=$pdo->query($all_news)->fetchAll();
 
 foreach($rows as $row){
     echo "<li class='list-group-item list-group-item-action d-flex'>";
+    echo "<div class='col-md-10'>";
     echo "<a href='index.php?do=news_detail&id={$row['id']}'>";
     echo $row['subject'];
     echo "</a>";
+    echo "</div>";
+    echo "<div class='col-md-2 text-center'>";
+    echo $row['readed'];
+    echo "</div>";
     echo "</li>";
 }
 
