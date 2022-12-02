4e8debd4fc8b38183d94f684291c121365aa545c
mackliu <mackliu@wda.gov.tw>
2022-12-02 16:33:44 +0800

    嘗試在首頁把原本的原生寫法取代為自訂函式的用法

--- front/main.php
+++ front/main.php
@@ -5,17 +5,21 @@
     <div class='col-md-2'>人氣</div>
 </li>    
 <?php
-//$all_news="SELECT * FROM `news` ";
-$all_news="SELECT * FROM `news`  ORDER by `top` desc,`readed` desc ";
-$rows=$pdo->query($all_news)->fetchAll();
-$hot=$pdo->query("SELECT `id` FROM `news` ORDER BY `readed` desc")->fetchColumn();
+
+/* $all_news="SELECT * FROM `news`  ORDER by `top` desc,`readed` desc ";
+$rows=$pdo->query($all_news)->fetchAll(); */
+
+$rows=all('news'," ORDER by `top` desc,`readed` desc");
+
+//$hot=$pdo->query("SELECT `id` FROM `news` ORDER BY `readed` desc")->fetchColumn();
+$hot=q("SELECT `id` FROM `news` ORDER BY `readed` desc")[0][0];
 foreach($rows as $row){
     echo "<li class='list-group-item list-group-item-action d-flex'>";
     echo "<div class='col-md-10'>";
     echo ($row['top']==1)?"top":'';
     echo ($row['id']==$hot)?"hot":'';
     echo "<a href='index.php?do=news_detail&id={$row['id']}'>";
-    echo $row['subject'];
+    echo $row['subject'];`
     echo "</a>";
     echo "</div>";
     echo "<div class='col-md-2 text-center'>";
