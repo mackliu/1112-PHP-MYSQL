5ef579e1fc8953a12dfd4aa03c01665dd8bcee50
mackliu <mackliu@wda.gov.tw>
2022-12-02 09:35:16 +0800

    在星星函式中加入形狀變數,讓一個函式可以畫多種形狀的星星

--- starts.php
+++ starts.php
@@ -2,25 +2,84 @@
 <pre>
 <?php
 
-starts(10);
-starts(5);
-starts(9);
-starts(12);
-starts(39);
+starts('正三角形',10);
+starts('菱形',11);
+starts('矩形',20);
+starts('直角三角形',12);
+starts('正三角形',20);
 
 
-function starts($size){
-    for($i=1 ; $i <= $size ;$i++){
+function starts($shape,$size){
+    switch($shape){
+        case "正三角形":
+            for($i=1 ; $i <= $size ;$i++){
     
-        for($k=1; $k <=($size-$i);$k++){
-            echo "&nbsp;";    
-        }
-        
-        for($j=1 ; $j <= (2*$i-1) ; $j++){
-            echo "*";
-        }
-        echo "<br>";
+                for($k=1; $k <=($size-$i);$k++){
+                    echo "&nbsp;";    
+                }
+                
+                for($j=1 ; $j <= (2*$i-1) ; $j++){
+                    echo "*";
+                }
+                echo "<br>";
+            }
+        break;
+        case "菱形":
+            $temp=0;
+            for($i=1;$i<=$size;$i++){
+             
+              if($i>ceil($size/2)){
+                $temp=$temp-1;
+              }else{
+                $temp=$i;
+              }
+             
+              for($j=1;$j<=(floor($size/2)+$temp);$j++){
+                if($j<=(ceil($size/2)-$temp)){
+                  echo "&nbsp;";
+                }else{
+                  echo "*";
+                }
+              }
+              echo "<br>";
+            }
+        break;
+        case "矩形":
+            for($i=1 ; $i<=$size ; $i++){
+
+                for( $j=1 ; $j<=$size ; $j++ ){
+                  
+                  if($i==1 || $i==$size){
+                    
+                    echo "*";
+                    
+                  }else if($j==1 || $j==$size){  
+                  
+                    echo "*";
+                    
+                  }else{
+                  
+                    echo "&nbsp;";
+                    
+                  }
+                  
+                }
+                
+                echo "<br>";
+              }
+        break;
+        case "直角三角形":
+            for($i=1;$i<=$size;$i++){
+
+                for($j=1;$j<=$i;$j++){ //在內圈中，我們把外圈的變數$i當成內圈次數的限制
+                    echo "*";
+                }
+                echo "<br>";
+            }
+            
+        break;
     }
+
 }
 
 
