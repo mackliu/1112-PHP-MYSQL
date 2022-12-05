340fdc36d35933353e3bc0afd0aa14dac918768a
mackliu <mackliu@wda.gov.tw>
2022-12-05 09:20:17 +0800

    建立問卷調查需要的主題資料表及項目資料表

---
+++ db/survey_subject.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-12-05 02:14:29
+-- 伺服器版本： 10.4.24-MariaDB
+-- PHP 版本： 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `survey_subject`
+--
+
+CREATE TABLE `survey_subject` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `subject` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `type` tinyint(1) UNSIGNED NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `vote` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `survey_subject`
+--
+ALTER TABLE `survey_subject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `survey_subject`
+--
+ALTER TABLE `survey_subject`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
