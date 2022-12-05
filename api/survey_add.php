b62b39d4f580b6accb7513ab46bedaf949f3f9e8
mackliu <mackliu@wda.gov.tw>
2022-12-05 10:40:52 +0800

    使用迴圈來完成選項新增的功能,並導回調查列表頁面

--- api/survey_add.php
+++ api/survey_add.php
@@ -5,16 +5,31 @@ include_once "../db/base.php";
 $type=1;
 $vote=0;
 $active=0; */
+//dd($_POST['subject']);
+//dd($_POST['opt']);
 //建立資料陣列
 $subject=['subject'=>$_POST['subject'],
           'type'=>1,
           'vote'=>0,
           'active'=>0];
-          
+
 //使用insert 函式，寫入資料
 insert('survey_subject',$subject);
 
-//insert('survey_options');
+$subject_id=find('survey_subject',['subject'=>$_POST['subject']])['id'];
 
+if(isset($_POST['opt'])){
+    foreach($_POST['opt'] as $option){
+        if($option!=''){
+            $tmp=['opt'=>$option,
+                  'subject_id'=>$subject_id,
+                  'vote'=>0];
+            dd($tmp);
+            insert('survey_options',$tmp);
+        } 
+    }
+}
+
+header("location:../admin_center.php?do=survey");
 
 ?>
