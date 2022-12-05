70e5582402800346a333fb3a8d461159f9de5d6b
mackliu <mackliu@wda.gov.tw>
2022-12-05 15:58:38 +0800

    製作前台投票列表頁面

--- front/survey.php
+++ front/survey.php
@@ -5,12 +5,25 @@
         <div class="col-2">參與數</div>
         <div class="col-2">操作</div>
     </li>
+
+<?php 
+    $surveys=all("survey_subject",['active'=>1]);
+    foreach($surveys as $survey){
+?>
     <li class="d-flex list-group-item list-group-item-light list-group-item-action">
-        <div class="col-8 font-weight-bolder" style="font-size:1.25rem">Lorem ipsum dolor sit</div>
-        <div class="col-2 text-center">0</div>
+        <div class="col-8 font-weight-bolder" style="font-size:1.25rem">
+            <?=$survey['subject'];?>
+        </div>
         <div class="col-2 text-center">
-            <a href="index.php?do=survey_item&id=" class="btn btn-sm btn-success mx-1">投票</a>
-            <a href="index.php?do=survey_result&id=" class="btn btn-sm btn-info mx-1">結果</a>
+            <?=$survey['vote'];?>
+        </div>
+        <div class="col-2 text-center">
+            <a href="index.php?do=survey_item&id=<?=$survey['id'];?>" class="btn btn-sm btn-success mx-1">投票</a>
+            <a href="index.php?do=survey_result&id=<?=$survey['id'];?>" class="btn btn-sm btn-info mx-1">結果</a>
         </div>
     </li>
+<?php 
+    }
+
+?>
 </ul>
