78830a1aae044e20c052b72cd8e327e2d694db5b
mackliu <mackliu@wda.gov.tw>
2022-12-05 11:35:26 +0800

    製作編輯調查的表單頁面,帶入各項個項目及主題的id

--- api/survey_edit.php
+++ api/survey_edit.php
@@ -2,6 +2,11 @@
 include_once "../db/base.php";
 
 
+dd($_POST['subject']);
+dd($_POST['subject_id']);
+dd($_POST['opt']);
+dd($_POST['opt_id']);
+
 
 
 ?>
--- back/survey_edit.php
+++ back/survey_edit.php
@@ -1,19 +1,34 @@
+<?php
+if(isset($_GET['id'])){
+    $subject=find('survey_subject',$_GET['id']);
+    //dd($subject);
+    $options=all('survey_options',['subject_id'=>$_GET['id']]);
+    //dd($options);
+}else{
+    header("location:admin_center.php?do=survey&error=沒有指定編輯的調查id");
+}
+
+?>
 <h3 class="text-center">編輯調查</h3>
 
 <form action="./api/survey_edit.php" class="col-10 mx-auto d-flex flex-wrap justify-content-end" method="post">
     <div class="form-group row col-12">
             <label class="col-2 text-right">主題</label>
-            <input type="text" class="form-control col-10">
+            <input type="text" name="subject" value="<?=$subject['subject'];?>" class="form-control col-10">
+            <input type="hidden" name="subject_id" value="<?=$subject['id'];?>">
         </div> 
         <!--選項區-->
+        <?php 
+        foreach($options as $idx => $option){
+        ?>
             <div class="form-group row col-11">
-                <label class="col-2 text-right">項目１</label>
-                <input type="text" class="form-control col-10">
+                <label class="col-2 text-right">項目<?=$idx+1;?></label>
+                <input type="text" name='opt[]' value="<?=$option['opt'];?>" class="form-control col-10">
+                <input type="hidden" name="opt_id[]" value="<?=$option['id'];?>">
             </div>    
-            <div class="form-group row col-11">
-                <label class="col-2 text-right">項目２</label>
-                <input type="text" class="form-control col-10">
-            </div>    
+        <?php 
+        }
+        ?>
 
     <div class="text-center col-12 mt-3">
         <input class="btn btn-primary mx-1" type="submit" value="修改">
