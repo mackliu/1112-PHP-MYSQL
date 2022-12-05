dcf28bf0c03b730aa0e34dff7d4905756058a4eb
mackliu <mackliu@wda.gov.tw>
2022-12-05 12:03:58 +0800

    完成編輯更新資料的功能

--- api/survey_edit.php
+++ api/survey_edit.php
@@ -1,12 +1,27 @@
 <?php
 include_once "../db/base.php";
 
-
 dd($_POST['subject']);
 dd($_POST['subject_id']);
+/* $subject=find('survey_subject',$_POST['subject_id']);
+dd($subject);
+if($subject['subect']!=$_POST['subject']){
+    //update('survey_subject',['subject'=>$_POST['subject']],$_POST['subject_id']);
+    
+} */
+update('survey_subject',['subject'=>$_POST['subject']],$_POST['subject_id']);
+
 dd($_POST['opt']);
 dd($_POST['opt_id']);
 
+foreach($_POST['opt_id'] as $idx => $id){
+    $option=find('survey_options',$id);
+    echo "<div> 原本的選項 $id 資料=>".$option['opt']."<br>";
+    echo "表單送過來的編輯後的選項 $id 資料=>".$_POST['opt'][$idx]."</div>";
+    update('survey_options',['opt'=>$_POST['opt'][$idx]],$id);
+}
 
+
+header("location:../admin_center.php?do=survey");
 
 ?>
