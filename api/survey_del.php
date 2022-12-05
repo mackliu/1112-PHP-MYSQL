a99c687c1ff6c3b233ca16252cd943975718b42a
mackliu <mackliu@wda.gov.tw>
2022-12-05 13:21:57 +0800

    完成刪除調查主題工作

--- api/survey_del.php
+++ api/survey_del.php
@@ -1,7 +1,19 @@
 <?php
 include_once "../db/base.php";
 
+//調查主題的id
+$id=$_GET['id'];
 
+//關聯的選項的id
+//$options=all("survey_options",['subject_id'=>$id]);
 
+del("survey_subject",$id);
 
+del("survey_options",['subject_id'=>$id]);
+
+/* foreach($options as $opt){
+    del("survey_options",$opt['id']);
+} */
+
+header("location:../admin_center.php?do=survey");
 ?>
