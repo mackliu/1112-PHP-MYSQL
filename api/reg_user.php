b63acff94a2fa58407319a1bf18184e499549186
mackliu <mackliu@wda.gov.tw>
2022-12-09 15:37:09 +0800

    解決註冊時,last_login 帶空值造成無法寫入資料表的問題

--- api/reg_user.php
+++ api/reg_user.php
@@ -5,9 +5,10 @@ $acc=trim(strip_tags($_POST['acc']));
 $pw=trim($_POST['pw']);
 $name=trim($_POST['name']);
 $email=trim($_POST['email']);
-$last_login=null;
+//$last_login="0000-00-00 00:00:00";  //移除,讓資料庫自動填入時間
 
-$sql="insert into `users` (`acc`,`pw`,`name`,`email`,`last_login`) values('$acc','$pw','$name','$email','$last_login')";
+//$sql="insert into `users` (`acc`,`pw`,`name`,`email`,`last_login`) values('$acc','$pw','$name','$email','$last_login')";
+$sql="insert into `users` (`acc`,`pw`,`name`,`email`) values('$acc','$pw','$name','$email')";
 echo "acc=>".$acc;
 echo "<br>";
 echo "pw=>".$pw;
@@ -16,6 +17,7 @@ echo "name=>".$name;
 echo "<br>";
 echo "email=>".$email;
 echo "<br>";
+//echo $sql;
 $pdo->exec($sql);
 
 //註冊完成後，將使用者導向登入頁
