4fe2d09a24b4391e5974cae048daa855560bbc88
mackliu <mackliu@wda.gov.tw>
2022-12-09 11:27:03 +0800

    增加log資料表，並在投票完成時增加一個log紀錄

--- api/survey_vote.php
+++ api/survey_vote.php
@@ -17,6 +17,26 @@ echo "<hr>"; */
 update("survey_subject",$subject,$subject['id']);
 update("survey_options",$option,$option['id']);
 
+//偵測使用者端IP,並取得IP
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+$log=[
+    'user'=>(isset($_SESSION['login']))?$_SESSION['login']['id']:0,
+    'ip'=>$ip,
+    'subject_id'=>$subject['id'],
+    'option_id'=>$option['id']
+];
+insert("survey_log",$log);
 to("../index.php?do=survey_result&id={$subject['id']}");
 
 ?>
---
+++ db/survey_log.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-12-09 04:26:23
+-- 伺服器版本： 10.4.24-MariaDB
+-- PHP 版本： 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `survey_log`
+--
+
+CREATE TABLE `survey_log` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user` int(11) NOT NULL,
+  `ip` varchar(16) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `option_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `survey_log`
+--
+ALTER TABLE `survey_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `survey_log`
+--
+ALTER TABLE `survey_log`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
